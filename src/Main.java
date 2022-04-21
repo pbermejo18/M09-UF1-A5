35aecaaeb57886a77ce391b9bec4cc3c0ca712f2
pbermejo18 <pbermejo@elpuig.xeill.net>
2022-04-21 15:47:10 +0200

    d

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.KeyPair;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Escriu un missatge: ");
+        String missatge = scanner.nextLine();
+
+        // genero la key
+        KeyPair k = Xifrar.randomGenerate(1028);
+
+        // xifro el missatge
+        System.out.println("*********** MISSATGE ENCRIPTAT ***********");
+        byte[] xifrat = Xifrar.encryptA5(missatge.getBytes(),k.getPublic());
+        String sx = new String(xifrat, StandardCharsets.UTF_8);
+        System.out.println(sx);
+
+        // desxifro el missatge
+        System.out.println("*********** MISSATGE DESENCRIPTAT ***********");
+        byte[] desxifrat = Xifrar.decryptA5(xifrat,k.getPrivate());
+        String sd = new String(desxifrat, StandardCharsets.UTF_8);
+        System.out.println(sd);
+
+        // k.getPublic() retorna la clau publica generada
+        // k.getPrivate() retorna la clau privada generada
+    }
+}
---
+++ src/Xifrar.java
@@ -0,0 +1,99 @@
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.*;
+import java.util.Arrays;
+
+public class Xifrar {
+    public static SecretKey keygenKeyGeneration(int keySize) {
+        SecretKey sKey = null;
+        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+            try {
+                KeyGenerator kgen = KeyGenerator.getInstance("AES");
+                kgen.init(keySize);
+                sKey = kgen.generateKey();
+
+            } catch (NoSuchAlgorithmException ex) {
+                System.err.println("Generador no disponible.");
+            }
+        }
+        return sKey;
+    }
+
+    public static SecretKey passwordKeyGeneration(String text, int keySize) {
+        SecretKey sKey = null;
+        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
+            try {
+                byte[] data = text.getBytes("UTF-8");
+                MessageDigest md = MessageDigest.getInstance("SHA-256");
+                byte[] hash = md.digest(data);
+                byte[] key = Arrays.copyOf(hash, keySize/8);
+                sKey = new SecretKeySpec(key, "AES");
+            } catch (Exception ex) {
+                System.err.println("Error generant la clau:" + ex);
+            }
+        }
+        return sKey;
+    }
+
+    public static byte[] encryptData(SecretKey sKey, byte[] data) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.ENCRYPT_MODE, sKey);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant les dades: " + ex);
+        }
+        return encryptedData;
+    }
+
+    public static byte[] decryptData(byte[] data, SecretKey key) {
+        byte[] decryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
+            cipher.init(Cipher.DECRYPT_MODE, key);
+            decryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error desxifrant les dades: " + ex);
+        }
+        return decryptedData;
+    }
+
+    public static KeyPair randomGenerate(int len) {
+        KeyPair keys = null;
+        try {
+            KeyPairGenerator keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(len);
+            keys = keyGen.genKeyPair();
+        } catch (Exception ex) {
+            System.err.println("Generador no disponible.");
+        }
+        return keys;
+    }
+
+    public static byte[] encryptA5(byte[] data, PublicKey pub) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.ENCRYPT_MODE, pub);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error xifrant: " + ex);
+        }
+        return encryptedData;
+    }
+
+    public static byte[] decryptA5(byte[] data, PrivateKey pub) {
+        byte[] encryptedData = null;
+        try {
+            Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding","SunJCE");
+            cipher.init(Cipher.DECRYPT_MODE, pub);
+            encryptedData =  cipher.doFinal(data);
+        } catch (Exception  ex) {
+            System.err.println("Error desxifrant: " + ex);
+        }
+        return encryptedData;
+    }
+}
