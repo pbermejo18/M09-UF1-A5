86a9bb716207a5987c219246299eab0709e4bdf3
pbermejo18 <pbermejo@elpuig.xeill.net>
2022-04-27 16:51:36 +0200

    1.2

--- src/Main.java
+++ src/Main.java
@@ -1,31 +1,50 @@
 import java.io.UnsupportedEncodingException;
 import java.nio.charset.StandardCharsets;
 import java.security.KeyPair;
+import java.security.KeyStore;
+import java.util.Enumeration;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws UnsupportedEncodingException {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Escriu un missatge: ");
-        String missatge = scanner.nextLine();
 
-        // genero la key
-        KeyPair k = Xifrar.randomGenerate(1028);
+        // 1.1
+            System.out.println("Escriu un missatge: ");
+            String missatge = scanner.nextLine();
 
-        // xifro el missatge
-        System.out.println("*********** MISSATGE ENCRIPTAT ***********");
-        byte[] xifrat = Xifrar.encryptA5(missatge.getBytes(),k.getPublic());
-        String sx = new String(xifrat, StandardCharsets.UTF_8);
-        System.out.println(sx);
+            // genero la key
+            KeyPair k = Xifrar.randomGenerate(1028);
 
-        // desxifro el missatge
-        System.out.println("*********** MISSATGE DESENCRIPTAT ***********");
-        byte[] desxifrat = Xifrar.decryptA5(xifrat,k.getPrivate());
-        String sd = new String(desxifrat, StandardCharsets.UTF_8);
-        System.out.println(sd);
+            // xifro el missatge
+            System.out.println("*********** MISSATGE ENCRIPTAT ***********");
+            byte[] xifrat = Xifrar.encryptA5(missatge.getBytes(),k.getPublic());
+            String sx = new String(xifrat, StandardCharsets.UTF_8);
+            System.out.println(sx);
 
-        System.out.println("*********** PUBLIC KEY / PRIVATE KEY ***********");
-        System.out.println(k.getPublic()); // retorna la informació de la clau publica generada
-        System.out.println(k.getPrivate()); // retorna la informació de la clau privada generada
+            // desxifro el missatge
+            System.out.println("*********** MISSATGE DESENCRIPTAT ***********");
+            byte[] desxifrat = Xifrar.decryptA5(xifrat,k.getPrivate());
+            String sd = new String(desxifrat, StandardCharsets.UTF_8);
+            System.out.println(sd);
+
+            System.out.println("*********** PUBLIC KEY / PRIVATE KEY ***********");
+            System.out.println(k.getPublic()); // retorna la informació de la clau publica generada
+            System.out.println(k.getPrivate()); // retorna la informació de la clau privada generada
+
+        // 1.2
+            try {
+                KeyStore keyStore = Xifrar.loadKeyStore("/home/usuario/.keystore","usuario");
+                char[] JavaCharArray = {'u', 's', 'u', 'a', 'r', 'i', 'o'};
+                Enumeration<String> enumeration = keyStore.aliases();
+                while(enumeration.hasMoreElements()) {
+                    String alias = enumeration.nextElement();
+                    System.out.println("alias name: " + alias);
+                }
+                System.out.println(keyStore.getKey("mykey", JavaCharArray));
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
     }
 }
--- src/Xifrar.java
+++ src/Xifrar.java
@@ -2,65 +2,12 @@ import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.io.FileInputStream;
 import java.security.*;
 import java.util.Arrays;
 
 public class Xifrar {
-    public static SecretKey keygenKeyGeneration(int keySize) {
-        SecretKey sKey = null;
-        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
-            try {
-                KeyGenerator kgen = KeyGenerator.getInstance("AES");
-                kgen.init(keySize);
-                sKey = kgen.generateKey();
-
-            } catch (NoSuchAlgorithmException ex) {
-                System.err.println("Generador no disponible.");
-            }
-        }
-        return sKey;
-    }
-
-    public static SecretKey passwordKeyGeneration(String text, int keySize) {
-        SecretKey sKey = null;
-        if ((keySize == 128)||(keySize == 192)||(keySize == 256)) {
-            try {
-                byte[] data = text.getBytes("UTF-8");
-                MessageDigest md = MessageDigest.getInstance("SHA-256");
-                byte[] hash = md.digest(data);
-                byte[] key = Arrays.copyOf(hash, keySize/8);
-                sKey = new SecretKeySpec(key, "AES");
-            } catch (Exception ex) {
-                System.err.println("Error generant la clau:" + ex);
-            }
-        }
-        return sKey;
-    }
-
-    public static byte[] encryptData(SecretKey sKey, byte[] data) {
-        byte[] encryptedData = null;
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, sKey);
-            encryptedData =  cipher.doFinal(data);
-        } catch (Exception  ex) {
-            System.err.println("Error xifrant les dades: " + ex);
-        }
-        return encryptedData;
-    }
-
-    public static byte[] decryptData(byte[] data, SecretKey key) {
-        byte[] decryptedData = null;
-        try {
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.DECRYPT_MODE, key);
-            decryptedData =  cipher.doFinal(data);
-        } catch (Exception  ex) {
-            System.err.println("Error desxifrant les dades: " + ex);
-        }
-        return decryptedData;
-    }
-
     public static KeyPair randomGenerate(int len) {
         KeyPair keys = null;
         try {
@@ -95,5 +42,15 @@ public class Xifrar {
             System.err.println("Error desxifrant: " + ex);
         }
         return encryptedData;
+    }
+
+    public static KeyStore loadKeyStore(String ksFile, String ksPwd) throws Exception {
+        KeyStore ks = KeyStore.getInstance("JKS");
+        File f = new File (ksFile);
+        if (f.isFile()) {
+            FileInputStream in = new FileInputStream (f);
+            ks.load(in, ksPwd.toCharArray());
+        }
+        return ks;
     }
 }
